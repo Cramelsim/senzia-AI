cdc18c49bfc0dff462531e9e95ecec100a69ded1
Melody Simwa <simwamelody@gmail.com>
2026-06-01 22:20:40 +0400

    update app.js

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Pricing from './pages/Pricing';
+import Demo from './pages/Demo';
+import './styles/globals.css';
