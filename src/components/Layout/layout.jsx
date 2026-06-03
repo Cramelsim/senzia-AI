5328beb22d48647ada31523bbaab1edb0633b7ff
Melody Simwa <simwamelody@gmail.com>
2026-06-03 21:12:57 +0400

    define layout in layout.jsx

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -2,14 +2,3 @@ import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
-const Layout = ({ children }) => {
-  return (
-    <div>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
-    </div>
-  );
-};
-
-export default Layout;
