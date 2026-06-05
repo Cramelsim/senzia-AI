f05c4a80b182642d389a90eae16fe0867fe9b24e
Melody Simwa <simwamelody@gmail.com>
2026-06-05 21:45:13 +0400

    add imports in home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+import Hero from '../components/Landing/Hero';
+import Features from '../components/Landing/Features';
+import Capabilities from '../components/Landing/Capabilities';
+import Pricing from '../components/Landing/Pricing';
+import Testimonials from '../components/Landing/Testimonials';
+
+const Home = () => {
+  return (
+    <Layout>
+      <Hero />
+      <Features />
+      <Capabilities />
+      <Pricing />
+      <Testimonials />
+    </Layout>
+  );
+};
+
+export default Home;
