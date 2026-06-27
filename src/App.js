7726a2aa067a8a643dd26f796dea4f93f9cca603
Melody Simwa <simwamelody@gmail.com>
2026-06-27 22:36:48 +0400

    add resources route in app.jsx

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/ai-consulting" element={<AIConsulting />} />
         <Route path="/contact" element={<Contact />} />
+                <Route path="/resources" element={<Resources />} />
       </Routes>
     </Router>
   );
