c64c9083fd6fe6cfe2378fc248347334b711b313
Melody Simwa <simwamelody@gmail.com>
2026-07-13 22:02:56 +0400

    define Navigate in demo.jsx

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, Navigate } from 'react-router-dom';
 import Navbar from '../components/Layout/Navbar';
 
 const Demo = ({ onLogin }) => {
